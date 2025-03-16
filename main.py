13819f5165347427ad2e2d1f7db8c56f4793facf
MUSTAFA892 <mustafatinwala6@gmail.com>
2025-03-16 15:36:25 +0530

    initialized the folder structure

---
+++ main.py
@@ -0,0 +1,28 @@
+from wsgiref import simple_server
+from flask import Flask
+from flask import Response
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/training', methods=['POST'])
+@cross_origin()
+
+def training_route_client():
+    try:
+        return Response("Training successfull")
+    except ValueError:
+        return Response("Error Occurred! %s" % ValueError)
+    except KeyError:
+        return Response("Error Occurred! %s" % KeyError)
+    except Exception as e:
+        return Response("Error Occurred! %s" % e)
+    
+if __name__ == '__main__':
+    #app.run(port=5000)
+    host= '0.0.0.0'
+    port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    httpd.serve_forever()
